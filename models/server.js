7b868f1a3765654ee9169112b1c69a8f86079bd2
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-11-23 07:57:59 -0600

    Seccion 3 finalizada

---
+++ models/server.js
@@ -0,0 +1,52 @@
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const path     = require('path');
+const Sockets  = require('./sockets');
+const cors     = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        
+        //HTTP server
+        this.server = http.createServer( this.app );
+        
+        // Configuracion de sockets
+        this.io = socketio(this.server, {cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+          }});
+    }
+    
+    middlewares(){
+        //Deplegar el directorio publico
+        this.app.use( express.static( path.resolve(__dirname, '../public' )));
+        
+        // CORS
+        
+        this.app.use( cors() );
+    }
+    
+    configurarSockets(){
+        new Sockets( this.io );
+    }
+    
+    execute(){
+        //Iniciarlizar middlewares
+        this.middlewares();
+        
+        
+        //Inicializar sockets
+        this.configurarSockets();
+        
+        //Inicialiazar server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto: ', this.port);
+        });    
+    }
+    
+}
+
+module.exports = Server;
